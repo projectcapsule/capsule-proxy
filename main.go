1d9b5020cc95cda8b89540ff50b5b93e9e2c40e8
Dario Tranchitella <dario@tranchitella.eu>
2020-09-18 14:26:39 +0200

    Doing bare reverse proxy with label selector

--- main.go
+++ main.go
@@ -33,9 +33,10 @@ func main() {
 	var mgr ctrl.Manager
 
 	fs := flag.NewFlagSet("filter", flag.ExitOnError)
-	listeningPort := fs.Uint("listening-port", 9001, "HTTP port the proxy listens to")
-	k8sControlPlaneUrl := fs.String("k8s-control-plane-url", "https://kubernetes.default.svc", "Kubernetes control plane URL")
-	capsuleUserGroup := fs.String("capsule-user-group", "clastix.capsule.io", "The Capsule User Group eligible to create Namespace for Tenant resources")
+	listeningPort := fs.Uint("listening-port", 9001, "HTTP port the proxy listens to (default: 9001)")
+	k8sControlPlaneUrl := fs.String("k8s-control-plane-url", "https://kubernetes.default.svc", "Kubernetes control plane URL (default: https://kubernetes.default.svc)")
+	capsuleUserGroup := fs.String("capsule-user-group", "clastix.capsule.io", "The Capsule User Group eligible to create Namespace for Tenant resources (default: clastix.capsule.io)")
+	usernameClaimField := fs.String("oidc-username-claim", "preferred_username", "The OIDC field name used to identify the user (default: preferred_username)")
 	err = fs.Parse(os.Args[1:])
 
 	opts := zap.Options{}
@@ -48,9 +49,10 @@ func main() {
 	}
 
 	log.Info("---")
-	log.Info(fmt.Sprintf("Manager will listen to port %d", *listeningPort))
+	log.Info(fmt.Sprintf("Manager listening on port %d", *listeningPort))
 	log.Info(fmt.Sprintf("Connecting to the Kubernete API Server listening on %s", *k8sControlPlaneUrl))
 	log.Info(fmt.Sprintf("The selected Capsule User Group is %s", *capsuleUserGroup))
+	log.Info(fmt.Sprintf("The OIDC username filed  %s", *usernameClaimField))
 	log.Info("---")
 
 	log.Info("Creating the manager")
@@ -65,7 +67,7 @@ func main() {
 
 	log.Info("Creating the Field Indexer")
 	ow := tenant.OwnerReference{}
-	err = mgr.GetFieldIndexer().IndexField(context.TODO(), ow.Object(), ow.Field(), ow.Func())
+	err = mgr.GetFieldIndexer().IndexField(context.Background(), ow.Object(), ow.Field(), ow.Func())
 	if err != nil {
 		log.Error(err, "cannot create new Field Indexer")
 		os.Exit(1)
@@ -73,7 +75,7 @@ func main() {
 
 	var r manager.Runnable
 	log.Info("Creating the NamespaceFilter runner")
-	r, err = webserver.NewKubeFilter(*listeningPort, *k8sControlPlaneUrl, *capsuleUserGroup, ctrl.GetConfigOrDie())
+	r, err = webserver.NewKubeFilter(*listeningPort, *k8sControlPlaneUrl, *capsuleUserGroup, *usernameClaimField, ctrl.GetConfigOrDie())
 	if err != nil {
 		log.Error(err, "cannot create NamespaceFilter runner")
 		os.Exit(1)
