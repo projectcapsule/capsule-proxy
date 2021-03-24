35298cd6b49176d163fa0a26015379574c48ba22
Maksim Fedotov <m_fedotov@wargaming.net>
2021-03-24 16:12:04 +0300

    refactor: remove unused Request interface methods

--- internal/request/http.go
+++ internal/request/http.go
@@ -25,18 +25,6 @@ func NewHttp(request *h.Request, usernameClaimField string) Request {
 	return &http{Request: request, usernameClaimField: usernameClaimField}
 }
 
-func (h http) IsNodeListing() (ok bool) {
-	ok = h.URL.Path == "/api/v1/nodes"
-	ok = (h.Method == "GET" || h.isWatchEndpoint()) && ok
-	return
-}
-
-func (h http) IsNamespaceListing() (ok bool) {
-	ok = h.URL.Path == "/api/v1/namespaces"
-	ok = (h.Method == "GET" || h.isWatchEndpoint()) && ok
-	return
-}
-
 func (h http) GetUserAndGroups() (username string, groups []string, err error) {
 	switch h.getAuthType() {
 	case certificateBased:
