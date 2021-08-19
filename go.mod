b0607921f8fb433e025ba80cb24fe1aaaef965c9
alegrey91 <ale_grey_91@hotmail.it>
2021-08-19 17:15:18 +0200

    feat: support for multiple Capsule groups

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
 	github.com/pkg/errors v0.9.1
+	github.com/spf13/pflag v1.0.5
 	go.uber.org/zap v1.15.0
 	k8s.io/api v0.20.2
 	k8s.io/apimachinery v0.20.2
--- internal/webserver/middleware/user_in_group.go
+++ internal/webserver/middleware/user_in_group.go
@@ -11,17 +11,19 @@ import (
 	req "github.com/clastix/capsule-proxy/internal/request"
 )
 
-func CheckUserInCapsuleGroupMiddleware(client client.Client, log logr.Logger, claim, group string, impersonate func(http.ResponseWriter, *http.Request)) mux.MiddlewareFunc {
+func CheckUserInCapsuleGroupMiddleware(client client.Client, log logr.Logger, claim string, groupNames []string, impersonate func(http.ResponseWriter, *http.Request)) mux.MiddlewareFunc {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 			_, groups, err := req.NewHTTP(request, claim, client).GetUserAndGroups()
 			if err != nil {
 				log.Error(err, "Cannot retrieve username and group from request")
 			}
-			if utils.NewUserGroupList(groups).Find(group) || utils.NewUserGroupList(groups).Find("system:serviceaccounts") {
-				next.ServeHTTP(writer, request)
+			for _, group := range groupNames {
+				if utils.NewUserGroupList(groups).Find(group) || utils.NewUserGroupList(groups).Find("system:serviceaccounts") {
+					next.ServeHTTP(writer, request)
 
-				return
+					return
+				}
 			}
 			log.V(5).Info("current user is not a Capsule one")
 			impersonate(writer, request)
