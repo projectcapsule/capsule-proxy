422f91d0bb5282d326629433d8a75ea0f75063d9
Gonzalo Gabriel Jiménez Fuentes <iam@mendrugory.com>
2021-10-18 10:22:46 +0200

    fix: binding servicceaccount of tests to capsule-apis clusterrole

--- e2e/libs/serviceaccount_utils.bash
+++ e2e/libs/serviceaccount_utils.bash
@@ -1,5 +1,35 @@
 #!/usr/bin/env bash
 
+function create_clusterrolebinding() {
+  local name namespace
+  name=${1}
+  namespace=${2}
+
+  cat <<EOF | kubectl apply -f -
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: ${namespace}:${name}
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: capsule-apis
+subjects:
+- kind: ServiceAccount
+  name: ${name}
+  namespace: ${namespace}
+EOF
+}
+
+function delete_clusterrolebinding() {
+  local name namespace
+  name=${1}
+  namespace=${2}
+
+  kubectl delete clusterrolebinding ${namespace}:${name}
+}
+
+
 function create_serviceaccount() {
   local name namespace
   name=${1}
@@ -14,6 +44,7 @@ metadata:
   namespace: ${namespace}
 EOF
 
+  create_clusterrolebinding ${name} ${namespace}
 
   SECRETNAME=$(kubectl get serviceaccounts -n ${namespace} ${name}  -o jsonpath='{.secrets[0].name}')
   TOKEN=$(kubectl get secret -n ${namespace} ${SECRETNAME} -o jsonpath='{.data.token}' | base64 -d)
