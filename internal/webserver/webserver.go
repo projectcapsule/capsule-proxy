f66b9ee5b9f0f9408f60a34fc3689961dcd4a571
Dario Tranchitella <dario@tranchitella.eu>
2022-05-09 14:44:54 +0200

    feat: support for owned namespace retrieval

--- internal/webserver/webserver.go
+++ internal/webserver/webserver.go
@@ -196,6 +196,7 @@ func (n kubeFilter) impersonateHandler(writer http.ResponseWriter, request *http
 func (n kubeFilter) registerModules(ctx context.Context, root *mux.Router) {
 	modList := []modules.Module{
 		namespace.List(n.roleBindingsReflector),
+		namespace.Get(n.roleBindingsReflector, n.client),
 		node.List(n.client),
 		node.Get(n.client),
 		ingressclass.List(n.client),
