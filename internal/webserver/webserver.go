f398827f368de8bc9abff038cfdef20d105509a2
Dario Tranchitella <dario@tranchitella.eu>
2022-06-16 17:12:09 +0200

    feat: support for user impersonation

--- internal/webserver/webserver.go
+++ internal/webserver/webserver.go
@@ -143,6 +143,10 @@ func (n kubeFilter) reverseProxyMiddleware(next http.Handler) http.Handler {
 
 // nolint:interfacer
 func (n kubeFilter) handleRequest(request *http.Request, selector labels.Selector) {
+	// Sanitizing the impersonation
+	request.Header.Del("Impersonate-User")
+	request.Header.Del("Impersonate-Group")
+
 	q := request.URL.Query()
 	if e := q.Get("labelSelector"); len(e) > 0 {
 		n.log.V(4).Info("handling current labelSelector", "selector", e)
