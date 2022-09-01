a83f87ab8a929e77f11b47f9682521eafb2ab808
Nick Van Rymenant <nick.vanrymenant@klarrio.com>
2022-09-01 16:14:05 +0200

    feat: support for gcp access tokens in auth middleware

--- internal/webserver/middleware/authorization_test.go
+++ internal/webserver/middleware/authorization_test.go
@@ -20,6 +20,7 @@ func TestCheckBearerToken(t *testing.T) {
 	}{
 		{"fail no bearer", "alksjdas239ldasdasd123ljksadsj", false, false},
 		{"pass jwt", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c", true, false},
+		{"pass gcp access token", "Bearer ya29.A0AVA9y1tO6PluMRS3RY2zq2SjrZxHoMFgcuubgHq-yUNsoiFd8WkLiVHoxU_LtgbrHaYImH8qsM8IMvodoIFsW9gxVvPU8df6Hi7qKn76bem8ifDZ6VkSGl93nmDiDoVOEIBMEUeOgzxoUCwkLr2iFMvmtzzIhWnBpQqzkNnPtLXNGNFNGNFSDR65qe8oSrRP756QYC9GEAmbl8KRt0173", true, false},
 		{"pass webtoken", "Bearer alksjdas2_9ldas-dasd123ljksadsj", true, false},
 		{"fail space in token", "Bearer alksjdas239ld asdasd123lksadsj", false, false},
 	}
