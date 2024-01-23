8bbce9a9ce015fecc298507ec1110f556f3d8b32
Dario Tranchitella <dario@tranchitella.eu>
2024-01-23 18:24:36 +0100

    feat: allow user to gets list of his tenants
    Co-authored-by: Anatoly Chernov <anatoly.chernov@gmail.com>
    Signed-off-by: Dario Tranchitella <dario@tranchitella.eu>

---
+++ internal/modules/tenants/get.go
@@ -0,0 +1,68 @@
+// Copyright 2020-2023 Project Capsule Authors.
+// SPDX-License-Identifier: Apache-2.0
+
+package tenants
+
+import (
+	"net/http"
+
+	"github.com/go-logr/logr"
+	"github.com/gorilla/mux"
+	capsulev1beta2 "github.com/projectcapsule/capsule/api/v1beta2"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/sets"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/projectcapsule/capsule-proxy/internal/modules"
+	"github.com/projectcapsule/capsule-proxy/internal/modules/errors"
+	"github.com/projectcapsule/capsule-proxy/internal/request"
+	"github.com/projectcapsule/capsule-proxy/internal/tenant"
+)
+
+type get struct {
+	capsuleLabel string
+	client       client.Reader
+	log          logr.Logger
+	gk           schema.GroupKind
+}
+
+func Get(client client.Reader) modules.Module {
+	label, _ := capsulev1beta2.GetTypeLabel(&capsulev1beta2.Tenant{})
+
+	return &get{
+		capsuleLabel: label,
+		client:       client,
+		log:          ctrl.Log.WithName("tenant_get"),
+		gk: schema.GroupKind{
+			Group: corev1.GroupName,
+			Kind:  "tenants",
+		},
+	}
+}
+
+func (l get) Path() string {
+	return "/apis/capsule.clastix.io/v1beta2/tenants/{name}"
+}
+
+func (l get) Methods() []string {
+	return []string{http.MethodGet}
+}
+
+func (l get) Handle(proxyTenants []*tenant.ProxyTenant, proxyRequest request.Request) (selector labels.Selector, err error) {
+	name := mux.Vars(proxyRequest.GetHTTPRequest())["name"]
+
+	userTenants := sets.New[string]()
+
+	for _, tnt := range proxyTenants {
+		userTenants.Insert(tnt.Tenant.Name)
+	}
+
+	if userTenants.Has(name) {
+		return labels.NewSelector(), nil
+	}
+
+	return nil, errors.NewNotFoundError(name, l.gk)
+}
